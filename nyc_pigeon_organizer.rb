0f7fa04935ac33fbc3df0238915bffd4adaa5b18
Austin Oie <austinoie@gmail.com>
2020-07-02 15:21:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   final_hash = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, name|
+      name.each do |
       if !final_hash[name]
         final_hash[name] = {}
       end 
