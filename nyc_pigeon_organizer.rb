e7e134f05decacb6f88dfd8d21a8dbc62782573b
Austin Oie <austinoie@gmail.com>
2020-07-02 15:10:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,8 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), final_array|
+  final_hash = data.each_with_object({}) do |(key, value), final_array|
   value.each do |inner_key, name|
   binding.pry 
-  puts data  
   end
-  binding.pry 
 end
